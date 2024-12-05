e995f9d17cc9afbb84090b9ffff1f989b7038115
pkulkarni35 <pkulkarni35@gmail.com>
2024-12-05 20:56:55 +0530

    committ

---
+++ TestProject1/Script/Unit2.js
@@ -0,0 +1,17 @@
+﻿function Test1() {
+  // Opens the specified URL in a running instance of the specified browser.
+  Browsers.Item(btChrome).Navigate("https://www.element34.com/");
+  Aliases.browser.BrowserWindow.Maximize();
+
+  // Interact with different elements on the page.
+  Aliases.browser.pageTestAutomationGridRunningIns.navSeleniumboxSbox.linkFeatures.Click();
+  Aliases.browser.pageTestAutomationGridRunningIns2.Wait();
+
+  Aliases.browser.pageTestAutomationGridRunningIns2.panelEnterpriseDeployment.Click();
+  Aliases.browser.pageTestAutomationGridRunningIns2.panelEnterpriseDeployment.Click();
+
+  Aliases.browser.pageTestAutomationGridRunningIns2.linkComprehensiveTesting.panelComprehensiveTesting.Click();
+  Aliases.browser.pageTestAutomationGridRunningIns2.linkAccessManagement.panelAccessManagement.Click();
+
+  Aliases.browser.pageTestAutomationGridRunningIns2.navSeleniumboxSbox.linkBookADemo.Click();
+}
